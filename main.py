614dc9b7f5d19b4a7514139874236f2e08e66574
Patryk Orłowski <orlowski.dev@gmail.com>
2025-07-01 05:48:02 +0200

    L8; venv changed to uv

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-    pygame.display.set_mode(screen_size)
-    surface = pygame.Surface(screen_size)
+    screen = pygame.display.set_mode(screen_size)
+    surface = pygame.Surface
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        surface.fill(color=(255, 255, 255, 1))
+        # surface.fill(color=(255, 255, 255, 1))
+        surface.fill(screen, color=(0, 0, 0, 1))
+
+        player.update(dt)
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
