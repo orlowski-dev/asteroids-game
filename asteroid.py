e95a69f2cf4add9330e5ba7d4a5deabed2fe8ac0
Patryk Orłowski <orlowski.dev@gmail.com>
2025-07-01 21:24:10 +0200

    ch3l1

---
+++ asteroid.py
@@ -0,0 +1,19 @@
+from circleshape import CircleShape
+from pygame.draw import circle
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, surface):
+        circle(
+            surface=surface,
+            color=(255, 255, 255, 1),
+            center=self.position,
+            radius=self.radius,
+            width=2,
+        )
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
+from asteroidfield import AsteroidField
 from player import Player
+from asteroid import Asteroid
 
 
 def main():
@@ -14,8 +16,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroid_filed = AsteroidField()
 
     while True:
         for event in pygame.event.get():
